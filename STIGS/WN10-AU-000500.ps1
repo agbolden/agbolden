0a01987215f2b744e0b1d38015e94133ab74e46f
agbolden <135596077+agbolden@users.noreply.github.com>
2025-05-26 18:57:24 -0400

    Update WN10-AU-000500.ps1

--- STIGS/WN10-AU-000500.ps1
+++ STIGS/WN10-AU-000500.ps1
@@ -25,7 +25,7 @@
     PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
 #>
 
-# YOUR CODE GOES HERE# Define the registry path and value
+Define the registry path and value
 $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
 $valueName = "MaxSize"
 $valueData = 32768  # 0x00008000 in hexadecimal
